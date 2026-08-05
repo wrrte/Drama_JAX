037a596dbd5e5dbf818e92490ef8099a3901ff86
choemj <wrrtewdqsw@gmail.com>
2026-08-05 14:47:52 +0900

    Drama는 이제 6번 쓰도록 수정

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 # 5번 GPU가 존재하는지 확인 (에러 없이 실행되면 존재하는 것)
-if nvidia-smi -i 5 >/dev/null 2>&1; then
-    export CUDA_VISIBLE_DEVICES=5
-    echo "✅ GPU 5 is detected. Using GPU 5 (Robone)."
+if nvidia-smi -i 6 >/dev/null 2>&1; then
+    export CUDA_VISIBLE_DEVICES=6
+    echo "✅ GPU 6 is detected. Using GPU 6 (Robone)."
 else
     export CUDA_VISIBLE_DEVICES=3
-    echo "⚠️ GPU 5 not found. Falling back to GPU 3 (B200)."
+    echo "⚠️ GPU 6 not found. Falling back to GPU 3 (B200)."
 fi
 export CUDA_LAUNCH_BLOCKING=1
 export MUJOCO_GL="egl"
 
 
-python train.py --BasicSettings.Env_name "ALE/Seaquest-v5"
 python train.py --BasicSettings.Env_name "ALE/Seaquest-v5"
 
 python train.py --BasicSettings.Env_name "ALE/Hero-v5"
